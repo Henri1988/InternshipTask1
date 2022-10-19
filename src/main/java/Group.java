e2362dbb02294d0dbbde0e7a74b0bbe86413f5ce
henrieessalu <henri.eessalu@gmail.com>
2022-10-19 19:31:34 +0300

    Amended Group entity

--- src/main/java/Group.java
+++ src/main/java/Group.java
@@ -11,6 +11,35 @@ public class Group {
     @Column(name = "name", nullable = false, length = 50)
     private String name;
 
+    public Group() {
+    }
+
+    public Group(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Group(String name) {
+        this.name = name;
+    }
 
 
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
