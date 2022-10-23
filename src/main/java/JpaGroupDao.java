6b3ffcd99c88dbd0183c66b816f2eb413427c05e
henrieessalu <henri.eessalu@gmail.com>
2022-10-23 16:40:55 +0300

    Made JpaGroupDao class

--- src/main/java/JpaGroupDao.java
+++ src/main/java/JpaGroupDao.java
@@ -1,22 +1,31 @@
 import jdk.javadoc.internal.doclets.toolkit.util.Group;
 import spi.GroupDao;
 
+import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
 
+
+@PersistenceContext
 public class JpaGroupDao extends GenericDaoJpaImpl<Group> implements GroupDao {
+
+    EntityManager em;
 
 
     public JpaGroupDao(Class<Group> persistentClass) {
         super(persistentClass);
     }
 
-    @PersistenceContext
     @Override
-    public Group save(Group entity) {
-        return null;
+    public Group save(Group group) {
+        em.getTransaction().begin();
+        em.persist(group);
+        em.getTransaction().commit();
+        return group;
     }
+
+
 
     @Override
     public Group update(Group entity) {
