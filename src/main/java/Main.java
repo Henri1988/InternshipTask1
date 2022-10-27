3f061a6e9b840877cee82180504d08cf327f9b6f
henrieessalu <henri.eessalu@gmail.com>
2022-10-28 00:56:46 +0300

    added org.slf4j dependency to pom.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import spi.Dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.logging.Logger;
@@ -11,23 +16,48 @@ public class Main {
     private static final Dao<User, Integer> USER_DAO = new UserDao();
 
 
-    public static void main(String[] args){
+    private static HikariDataSource dataSource = null;
 
-        //POST
-        User firstUser =
-                new User("henri.eessalu@gmail.com", "Henri", "Eessalu", "upkeep123", "+37256640162", "Urban Architecture AS", "CEO", 3);
-        addUser(firstUser).ifPresent(firstUser::setId);
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
+        config.setUsername("postgres");
+        config.setPassword("postgres");
+        config.addDataSourceProperty("minimumIdle", "5");
+        config.addDataSourceProperty("maximumPoolSize", "25");
 
-        //GET
-        getAllUsers().forEach(System.out::println);
+        dataSource = new HikariDataSource(config);
+    }
 
-        //PUT
-        firstUser.setFirstName("Franklin");
-        firstUser.setLastName("Lewis");
-        updateUser(firstUser);
 
-        //DELETE
-        deleteUser(firstUser);
+    public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+
+
+        try {
+            connection = dataSource.getConnection();
+
+            //POST
+            User firstUser =
+                    new User("henri.eessalu@gmail.com", "Henri", "Eessalu", "upkeep123", "+37256640162", "Urban Architecture AS", "CEO", 3);
+            addUser(firstUser).ifPresent(firstUser::setId);
+
+            //GET
+            getAllUsers().forEach(System.out::println);
+
+            //PUT
+            firstUser.setFirstName("Franklin");
+            firstUser.setLastName("Lewis");
+            updateUser(firstUser);
+
+            //DELETE
+            deleteUser(firstUser);
+
+
+        } finally {
+            assert connection != null;
+            connection.close();
+        }
     }
 
     public static Optional<Integer> addUser(User user) {
