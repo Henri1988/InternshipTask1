b5c6c2627ab35b71b018956888f35cd01bcda161
henrieessalu <henri.eessalu@gmail.com>
2022-11-01 11:39:02 +0200

    amended persistence.xml

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,9 +1,15 @@
+import entities.User;
 import org.springframework.context.annotation.Configuration;
 
 
 
 @Configuration
 public class AppConfig {
+
+    public JpaUserDao userDaoBean (){
+        return new JpaUserDao(userDaoBean().getPersistentClass());
+
+    }
 
 
 }
